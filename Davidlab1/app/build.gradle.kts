a9a31af636c2a82122617c4f138f9bd2847f0466
David <daveedtee5@gmail.com>
2026-01-16 09:11:07 -0500

    fixed

--- Davidlab1/app/build.gradle.kts
+++ Davidlab1/app/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
 
 android {
     namespace = "david.odulana.n0155090"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "david.odulana.n0155090"
